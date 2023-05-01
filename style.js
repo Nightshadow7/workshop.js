5fb422db6310faea7ef5af2a4cf68a5e30f01616
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-01 18:41:33 -0500

    feat :sparkles: Taller 2 Ejercicio-4 Con el nombre y la edad de 3 personas calculo cual es el mayor de todos.

--- style.js
+++ style.js
@@ -1,6 +1,17 @@
-// 3. Contruir el algoritmo para determinar el voltaje de un circuito a partir de la resistencia y la intensidad de la corriente 
+// 4. Construir el algoritmo que solicite el nombre y edad de 3 personas y determine el nombre de la persona de mayor edad
 
-let resistencia = parseFloat(prompt(`Digite la resistencia`));
-let intensidad = parseFloat(prompt(`Digite la intensidad`));
-let voltaje = resistencia * intensidad;
-alert(`El voltaje es ${voltaje} voltios`);
+let nombre1 = prompt(`Digite su nombre`);
+let edad1 = prompt(`Hola ${nombre1} digite su edad`);
+let nombre2 = prompt(`Digite su nombre`);
+let edad2 = prompt(`Hola ${nombre2} Digite su edad`);
+let nombre3 = prompt(`Digite su nombre`);
+let edad3 = prompt(`Hola ${nombre3} Digite su edad`);
+if (edad1 > edad2 && edad1 > edad3){
+  alert(`El nombre de la persona de mayor edad es ${nombre1}`);
+}
+else if (edad2 > edad1 && edad2 > edad3){
+  alert(`El nombre de la persona de mayor edad es ${nombre2}`);
+}
+else if (edad3 > edad1 && edad3 > edad2){
+  alert(`El nombre de la persona de mayor edad es ${nombre3}`);
+}
