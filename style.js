d359c599df7768202beeaa1f901654a7ace0c333
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-01 18:37:31 -0500

    feat :sparkles: Taller 2 Ejercicio-2 Se indica si un numero es par o impar y si es mayor que 10

--- style.js
+++ style.js
@@ -1,11 +1,17 @@
-//1. Construir un algoritmo paa un programa que ingrese tres notas del alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "estudie", de lo contrario un mensaje que diga "becado".
+//2. Dado un numero indicar si es par o impar y si es mayor que 10
 
-let nota1 = parseFloat(prompt(`Digite su nota 1`));
-let nota2= parseFloat(prompt(`Digite su nota 2`));
-let nota3 = parseFloat(prompt(`Digite su nota 3`));
-let promedio = (nota1 + nota2 + nota3) / 3;
-if (promedio >= 3.9) {
-  alert(`Su promedio de las notas es ${promedio}\n ¡Estudie!`);
-} else {
-  alert(`Su promedio de las notas es ${promedio}\n ¡Becado!`);
+let numero = parseFloat(prompt(`Digite un numero`));
+if (numero >= 10) {
+  if (numero % 2 === 0){
+    alert("es mayor que 10 y es par");
+  }
+  else {
+    alert("es mayor que 10 y es impar");
+  }
+} else{
+  if (numero % 2 === 0){
+    alert("es menor que 10 y es par");
+  } else{
+    alert("es menor que 10 y es impar");
+  }
 }
