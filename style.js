109d218c2a3a8c97f3f9587385bc283324bd2ca2
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-01 22:11:44 -0500

    feat :sparkles: Taller 2 Ejercicio-10 Programa repetitivo del ingreso de numeros, que al dijitar el numero 0 termine el proceso y me muestre suma, promedio, cuantos numeros dijitados,mayor y menor valor.

--- style.js
+++ style.js
@@ -1,21 +1,29 @@
-//9. N atletas han pasado a finales en salto triple en los juegos olímpicos femenino de 2022. Diseñe un programa que pida por teclado los nombres de cada atleta finalista y a su vez, sus marcas del salto en metros. Informar el nombre de la atleta campeona que se quede con la medalla de oro y si rompió récord, reportar el pago que será de 500 millones. El récord esta en 15,50 metros.
+//10. 10. Desarrolle un programa cíclico que capture un dato numérico cada vez, y los vaya acumulando. El programa se detiene cuando el usuario digita un cero. El programa debe mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR VALOR Y MENOR VALOR.
 
-let atletas = [];
-let cantidadFinalistas = parseInt(prompt(`Dijite la cantidad de finalistas del salto triple`))
-for (let i = 0; i <cantidadFinalistas;i++){
-  let nombre = prompt(`Dijite el nombre del finalista: `);
-  nombre = nombre.replace(/\b\w/g, l => l.toUpperCase());
-  let marca = parseFloat(prompt(`Dijite la marca de ${nombre} en el salto triple `));
-  atletas.push({
-    nombre: nombre,
-    marca: marca
-  });
-}
-let finalistaMarca = atletas.reduce(function(a,b){ 
-  return (a.marca > b.marca) ? a : b;
-})
-if  (finalistaMarca > 15.5){
-  alert(`La finalista ${finalistaMarca.nombre} rompio el record de 15.50 metros con una marca de ${finalistaMarca.marca} metros. ¡Gano la medalla de oro!`)
-} else {
-  alert (`Nadie rompio la meta de 15.5 metros\n El atleta con mayor marca es ${finalistaMarca.nombre} con una marca de ${finalistaMarca.marca} metros`)
-}
+let number = [];
+ while (true) {
+  let numero = parseFloat(prompt(`Dijite un numero cualquiera:\n Dijite 0 para detener el proceso`));
+
+  if (numero == 0){
+    break
+  } else {
+    number.push(numero);
+  }
+ }
+ let mayorValor = number.reduce(function(a,b){
+  return (a > b) ? a : b;
+ });
+ let menorValor = number.reduce(function(a,b){
+  return (a < b) ? a : b;
+ });
+ let cantidad = number.length
+ let suma = number.reduce(function(a,b){
+  return a + b;
+ });
+ let promedio = suma / cantidad;
+ console.log(number)
+ console.log(`La sumatoria de los ${cantidad.toLocaleString()} valores ingresados es: ${suma.toLocaleString()}`);
+ console.log(`El valor promedio de los valores ingresados es ${promedio.toLocaleString()} `);
+ console.log(`Se dijitaron un total de ${cantidad} valores `);
+ console.log(`El mayor numero dijitado es: ${mayorValor}`);
+ console.log(`El menor numero dijitado es: ${menorValor}`);
