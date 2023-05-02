ebf0d516029e2e0c986b9d0990601c47c2f0dad9
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-01 21:45:15 -0500

    feat :sparkles: Taller 2 Ejercicio-9 Programa que ingresado una cantidad de atletas me muestre el atleta con la mayor marca y si rompio el record

--- style.js
+++ style.js
@@ -1,11 +1,21 @@
-//8. Programa que Ingrese por teclado: a. el valor del lado de un cuadrado para mostrar por pantalla el perímetro del mismo b. la base y la altura de un rectángulo para mostrar el área del mismo
-alert(`Programa que calcula el area de un cuadrado`)
-let lado = parseFloat(prompt(`Dijite el lado del cuadrado`))
-let area = lado * lado;
-alert(`El area del cuadrado con lado ${lado.toLocaleString()} es ${area.toLocaleString()}.`)
+//9. N atletas han pasado a finales en salto triple en los juegos olímpicos femenino de 2022. Diseñe un programa que pida por teclado los nombres de cada atleta finalista y a su vez, sus marcas del salto en metros. Informar el nombre de la atleta campeona que se quede con la medalla de oro y si rompió récord, reportar el pago que será de 500 millones. El récord esta en 15,50 metros.
 
-alert(`Programa que calcula el area del rectangulo.`)
-let base = parseFloat(prompt(`Dijite la base del rectangulo:  `))
-let altura = parseFloat(prompt(`Dijite la altura del rectangulo: `));
-let area2= base * altura;
-alert(`El area del rectangulo con lados ${base.toLocaleString()} de base y ${altura.toLocaleString()} de altura es: ${area2.toLocaleString()}. `)
+let atletas = [];
+let cantidadFinalistas = parseInt(prompt(`Dijite la cantidad de finalistas del salto triple`))
+for (let i = 0; i <cantidadFinalistas;i++){
+  let nombre = prompt(`Dijite el nombre del finalista: `);
+  nombre = nombre.replace(/\b\w/g, l => l.toUpperCase());
+  let marca = parseFloat(prompt(`Dijite la marca de ${nombre} en el salto triple `));
+  atletas.push({
+    nombre: nombre,
+    marca: marca
+  });
+}
+let finalistaMarca = atletas.reduce(function(a,b){ 
+  return (a.marca > b.marca) ? a : b;
+})
+if  (finalistaMarca > 15.5){
+  alert(`La finalista ${finalistaMarca.nombre} rompio el record de 15.50 metros con una marca de ${finalistaMarca.marca} metros. ¡Gano la medalla de oro!`)
+} else {
+  alert (`Nadie rompio la meta de 15.5 metros\n El atleta con mayor marca es ${finalistaMarca.nombre} con una marca de ${finalistaMarca.marca} metros`)
+}
