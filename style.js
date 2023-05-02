2c63de6198d96fb5a5b381b7bf1566d3ec354444
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-01 21:19:26 -0500

    feat :sparkles: Taller 2 Ejercicio-8 Programa que calcula el area de un cuadrado y de un rectangulo.

--- style.js
+++ style.js
@@ -1,9 +1,11 @@
-//7. Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura.
+//8. Programa que Ingrese por teclado: a. el valor del lado de un cuadrado para mostrar por pantalla el perímetro del mismo b. la base y la altura de un rectángulo para mostrar el área del mismo
+alert(`Programa que calcula el area de un cuadrado`)
+let lado = parseFloat(prompt(`Dijite el lado del cuadrado`))
+let area = lado * lado;
+alert(`El area del cuadrado con lado ${lado.toLocaleString()} es ${area.toLocaleString()}.`)
 
-let nombre = prompt(`Digite el nombre del articulo: `);
-let precio = parseFloat(prompt(`Digite el precio de l@s ${nombre}: `));
-let cantidad = parseInt(prompt(`Digite la cantidad de ${nombre} que se desea llevar: `));
-
-let cant = cantidad * precio;
-
-alert(`El comprador debe pagar un total de $${cant.toLocaleString()} por una cantidad de ${cantidad} ${nombre} a un precio por unidad de $${precio.toLocaleString()}`)
+alert(`Programa que calcula el area del rectangulo.`)
+let base = parseFloat(prompt(`Dijite la base del rectangulo:  `))
+let altura = parseFloat(prompt(`Dijite la altura del rectangulo: `));
+let area2= base * altura;
+alert(`El area del rectangulo con lados ${base.toLocaleString()} de base y ${altura.toLocaleString()} de altura es: ${area2.toLocaleString()}. `)
