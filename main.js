f1759ab10ed2823a9a4064f5fdf1d890eb61acde
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-04 07:49:58 -0500

    feat :hammer: Modificaccion del scrip añadi la estructura incompleta del objeto campus

---
+++ main.js
@@ -0,0 +1,119 @@
+let campus={
+    Campus : {
+        numSedes : 4,
+        sedes: {
+            sede1 : {
+                nombre: "Bucaramanga",
+                contacto : {
+                    telefono: 3118807659,
+                    whatsApp: 3131414,
+                    direccion: "Santander,Bucaramanga, Zona Franca Santander KM 4 Anillo Vial, Torre Zenith Piso 6",
+                }
+            },
+            sede2: {
+                nombre: "Bogota",
+                contacto : {
+                    telefono: 3118807659,
+                    whatsApp: 3131414,
+                    direccion: "Santander,Bucaramanga, Zona Franca Santander KM 4 Anillo Vial, Torre Zenith Piso 6",
+                }
+            
+            },
+            sede3: {
+                nombre: "Medellin",
+                contacto : {
+                    telefono: 3118807659,
+                    whatsApp: 3131414,
+                    direccion: "Santander,Bucaramanga, Zona Franca Santander KM 4 Anillo Vial, Torre Zenith Piso 6",
+                }
+            },
+            sede4: {
+                nombre: "Mexico",
+                contacto : {
+                    telefono: 3118807659,
+                    whatsApp: 3131414,
+                    direccion: "Santander,Bucaramanga, Zona Franca Santander KM 4 Anillo Vial, Torre Zenith Piso 6",
+                }
+            },
+            
+        }
+    },
+    trainers : {
+        trainers: 3,
+        nombres : {
+            sputnik: "Miguel",
+            artemist: "Vermen",
+            apolo: "Johlver",
+        },
+        informacionTrainers:{
+            sputnik: {
+                name: "Miguel Castro",
+                numero: 3045641916,
+                email: "ma980309651@gmail.com",
+                nivel: "Avanzado",
+                salon: 1,
+                jornada: "Mañana",
+                piso: 6,
+                teams: {
+                    M1: {
+                        name: "The end of the backend 3.0",
+                        horario: "6:00 am - 9:30 am",
+                    },
+                    M2: {
+                        name: "Sput Developments",
+                        horario: "10:20 am - 2:00 pm",
+                    },
+                    M3: {
+                        name: "Team 3",
+                        horario: "2:00 pm - 5:30 pm"
+                    }
+                }
+            },
+            artemist: {
+                name: "Vermen Ayala",
+                numero: 3208502824,
+                email: "vermenayala@campuslands.com",
+                nivel: "Intermedio",
+                salon: 2,
+                jornada: "Mañana",
+                piso: 6,
+                teams: {
+                    V1: {
+                        name: "Artemist 1",
+                        horario: "6:00 am - 9:30 am",
+                    },
+                    V2: {
+                        name: "Artemist 2",
+                        horario: "10:20 am - 2:00 pm",
+                    },
+                }
+            },
+            apolo: {
+                name: "Johver Pardo",
+                numero: 3058194054,
+                email: "johlverjosepardogarcia@gmail",
+                nivel: "Basico",
+                salon: 3,
+                jornada: "Mañana",
+                piso: 6,
+                teams: {
+                    J1: {
+                        name: "Team 1",
+                        horario: "6:00 am - 9:30 am",
+                    },
+                    J2: {
+                        name: "Team 2",
+                        horario: "10:20 am - 2:00 pm",
+                    },
+                }
+            },
+        }
+    },
+    campers : {
+        cantidad: 400,
+        teams : function (){
+            
+        }
+        
+    }
+}
